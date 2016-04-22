a5660c554c27adc7133788498d19f3345aae6931
Chris Reeves <chris@ev-soft.net>
2016-04-22 15:16:05 -0400

    borrow memory test from @sheershoff in #2

--- test/index.js
+++ test/index.js
@@ -1,5 +1,7 @@
 var should = require('should');
 var xoss = require(__dirname + '/../');
+var util = require('util');
+var Readable = require('stream').Readable;
 
 var xml = '<library><shelf id="returns" /><section name="fiction"><shelf id="1023">' +
             '<book title="A Chai Too Hot"><author>TJ Hollowaychuk</author><pages>411</pages></book>' +
@@ -101,5 +103,62 @@ describe('XML Stream', function() {
         done();
       });
     });
+  });
+
+  it('should discard text for irrelevant nodes', function(done) {
+    this.timeout(10000);
+    var iterations = 4000;
+    var leakdata = 'AAAAAAAAAA'.repeat(4096);
+    var xos = xoss();
+    var oldMem;
+    var newMem;
+
+    global.gc();
+    oldMem = process.memoryUsage();
+
+    var MyStream = function(options) {
+      Readable.call(this, options); // pass through the options to the Readable constructor
+      this.headPushed = false;
+      this.counter = iterations;
+      this.proceeding = true;
+    };
+
+    util.inherits(MyStream, Readable); // inherit the prototype methods
+
+    MyStream.prototype._read = function(n) {
+      var self = this;
+      if (!this.headPushed) {
+        this.push('<library><shelf>123</shelf>');
+        this.headPushed = true;
+      } else {
+        if(self.proceeding && self.counter > 1){
+          self.push(leakdata);
+        }
+        if(self.counter === 1){
+          global.gc();
+          newMem = process.memoryUsage();
+          if(self.proceeding){
+            self.push('</library>');
+          }
+        }
+        if (self.counter-- <= 0) { // stop the stream
+          if(self.proceeding){
+            self.proceeding = false;
+            self.push(null);
+          }
+        }
+      }
+    };
+
+    var xmlFeeder = new MyStream();
+    xos(xmlFeeder, ['/library/shelf']).then(function(s) {
+      try {
+        newMem.heapTotal.should.be.belowOrEqual(oldMem.heapTotal + leakdata.length*iterations, 'heapTotal');
+        newMem.heapUsed.should.be.belowOrEqual(oldMem.heapUsed + leakdata.length*iterations, 'heapUsed');
+        done();
+      }catch (e) {
+        done(e);
+      }
+    }, done);
   });
 });
